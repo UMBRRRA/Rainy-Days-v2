a249819397d545c9995e6683ae7d00bdc5a44e57
Jakub Ambroziak <ambroziak666@gmail.com>
2022-09-03 20:31:57 +0200

    hover button

--- Rainy Days v2/Assets/Scripts/MyButton.cs
+++ Rainy Days v2/Assets/Scripts/MyButton.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MyButton : MonoBehaviour
+public class MyButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private Button btn;
+    //private Button btn;
+    private Text text;
+    public Color normalColor;
+    public Color hoverColor;
 
     void Awake()
     {
-        btn = GetComponent<Button>();
+        //btn = GetComponent<Button>();
+        text = GetComponentInChildren<Text>();
+        text.color = normalColor;
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = hoverColor;
+    }
 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = normalColor;
+    }
 
 }
