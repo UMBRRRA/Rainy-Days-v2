c8fecb7d849919374740329a8aa4d26eec6d303f
Jakub Ambroziak <ambroziak666@gmail.com>
2022-09-05 21:29:12 +0200

    woods level

--- Rainy Days v2/Assets/Scripts/OccupyingStaticObject.cs
+++ Rainy Days v2/Assets/Scripts/OccupyingStaticObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OccupyingStaticObject : MonoBehaviour
 {
     private MapManager mapManager;
+    public List<Vector3Int> additionalFields = new();
 
     void Start()
     {
@@ -18,6 +19,13 @@ public class OccupyingStaticObject : MonoBehaviour
         Vector3Int myGridPos = mapManager.map.WorldToCell(myZ0transform);
         Vector3Int gridZ0 = new Vector3Int(myGridPos.x, myGridPos.y, 0);
         mapManager.OccupiedFields.Add(gridZ0);
+        if (additionalFields.Count != 0)
+        {
+            foreach (Vector3Int field in additionalFields)
+            {
+                mapManager.OccupiedFields.Add(new Vector3Int(field.x, field.y, 0));
+            }
+        }
     }
 
 }
---
+++ Rainy Days v2/Assets/Scripts/RandomStartAnim.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStartAnim : MonoBehaviour
+{
+    private Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(WaitRandom());
+    }
+
+    public IEnumerator WaitRandom()
+    {
+        yield return new WaitForSeconds(Random.Range(0, 5));
+        animator.SetBool("randomTime", true);
+    }
+}
